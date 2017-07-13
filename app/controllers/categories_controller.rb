f1243ee482eee2df71b543df2fddae832521fe05
Radoslav Petrov <radoslav.p.petrov@gmail.com>
2017-07-13 13:54:45 +0300

    Remove code duplication from categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
+  before_action :set_category, only: [:edit, :update, :show]
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -20,11 +21,9 @@ class CategoriesController < ApplicationController
   end
 
   def edit
-    @category = Category.find(params[:id])
   end
 
   def update
-    @category = Category.find(params[:id])
     if @category.update(category_params)
       flash[:success] = 'Category name was successfully updated!'
       redirect_to category_path(@category)
@@ -34,7 +33,6 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -49,6 +47,10 @@ class CategoriesController < ApplicationController
       flash[:danger] = 'Only admins can perform that action!'
       redirect_to categories_path
     end
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 
 end
